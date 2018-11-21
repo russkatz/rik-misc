cb0865509a2304b9eb4872a51f566702f14b1b49
russkatz@gmail.com <lvfrogtech@gmail.com>
2018-11-21 12:50:19 -0500

    lots of updates

--- do-rstbot.py
+++ do-rstbot.py
@@ -8,8 +8,8 @@ from dse.cluster import Cluster, ExecutionProfile, EXEC_PROFILE_DEFAULT
 from dse.auth import PlainTextAuthProvider
 
 #Configuration
-contactpoints = ['127.0.0.1']
-auth_provider = PlainTextAuthProvider (username='sd', password='sdfsdf')
+contactpoints = ['6.7.11.9']
+auth_provider = PlainTextAuthProvider (username='travian', password='sdfsdfgsdf')
 
 cluster = Cluster( contact_points=contactpoints,
                    auth_provider=auth_provider
@@ -28,29 +28,22 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    relaychan = discord.Object(id='123')
-    #relaychan = message.channel
+    #relaychan = discord.Object(id='dfgsdgsdf')
+    relaychan = message.channel
     if message.author == client.user:
         return
     if message.content.startswith('!test'):
         msg = "Hello, <@%s>" % (message.author.id)
         await client.send_message(message.channel, msg)
 
-    elif message.content.startswith('!send'):
-        print("!send")
+    elif message.content.startswith('!status'):
+        print("!status")
         input = message.content.split()
-        if len(input) != 6:
-            await client.send_message(message.channel, 'Usage: !send <reqid> <wood> <clay> <iron> <crop>')
+        if len(input) != 2:
+            await client.send_message(message.channel, 'Usage: !status <reqid>')
             return
-        reqid = str(input[1])
-        wood = str(input[2])
-        clay = str(input[3])
-        iron = str(input[4])
-        crop = str(input[5])
-        query = """ INSERT INTO travian.requests (reqid, status) VALUES (%s, 'open') """ % (int(reqid))
-        print(query)
-        session.execute (query)
-        query = """ select reqid, wood, woodsent, clay, claysent, iron, ironsent, crop, cropsent, player, village from travian.requests WHERE reqid = %s """ % (reqid)
+        reqid = input[1]
+        query = """ select reqid, wood, woodsent, clay, claysent, iron, ironsent, crop, cropsent, player, village, status from travian.requests WHERE reqid = %s """ % (reqid)
         results = session.execute (query)
         for row in results:
             reqid = row[0]
@@ -64,6 +57,100 @@ async def on_message(message):
             cropsent = row[8]
             player = row[9]
             village = row[10]
+            status = row[11]
+            xy = village.split(',')
+            msg = """ReqID: %s/%s Player: %s (https://ts1.travian.us/karte.php?x=%s&y=%s)\n Wood: %s/%s\n Clay: %s/%s\n Iron %s/%s\n Crop: %s/%s  """ % (reqid, status, player, xy[0], xy[1], woodsent, wood, claysent, clay, ironsent, iron, cropsent, crop)
+            await client.send_message(message.channel, msg)
+
+    elif message.content.startswith('!list'):
+        print("!list")
+        input = message.content.split()
+        query = """SELECT player, xy, name FROM travian.villages WHERE player = '%s'""" % (message.author.id)
+        results = session.execute (query)
+        msg = ""
+        for row in results:
+            msg = msg + "\n" + row[1] + " " + row[2]
+        await client.send_message(message.channel, msg)
+
+    elif message.content.startswith('!del'):
+        print("!del")
+        input = message.content.split()
+        if len(input) != 2:
+            await client.send_message(message.channel, 'Usage: !del <village x,y>')
+            return 
+        village = str(input[1])
+        village = re.sub('[\(\)\{\}<>]', '', village)
+        xy = village.split(',')
+        if len(xy) != 2:
+            xy = village.split('|')
+        if len(xy) != 2:
+            await client.send_message(message.channel, 'Invalid village coordinates!')
+            return
+        xy = village.split(',')
+        village = str(xy[0]) + "," + str(xy[1])
+        player = message.author.id
+        query = """DELETE FROM travian.villages WHERE player = '%s' AND xy = '%s'""" % (player, village)
+        session.execute (query)
+        msg = """Deleted %s""" % (village)
+        await client.send_message(message.channel, msg)
+
+    elif message.content.startswith('!add'):
+        print("!add")
+        input = message.content.split()
+        if len(input) < 2:
+            await client.send_message(message.channel, 'Usage: !add <village x,y> <name>')
+            return
+        village = str(input[1])
+        village = re.sub('[\(\)\{\}<>]', '', village)
+        xy = village.split(',')
+        if len(xy) != 2:
+            xy = village.split('|')
+        if len(xy) != 2:
+            await client.send_message(message.channel, 'Invalid village coordinates!')
+            return
+        xy = village.split(',')
+        village = str(xy[0]) + "," + str(xy[1])
+        player = message.author.id
+        input.pop(0)
+        input.pop(0)
+        name = ""
+        for n in input:
+            name = name + " " + n
+        query = """ INSERT INTO travian.villages (player, xy, name) VALUES ('%s', '%s', '%s') """ % (player, village, name)
+        session.execute (query)
+        msg = """Added %s (%s)""" % (name, village)
+        await client.send_message(message.channel, msg)
+
+    elif message.content.startswith('!send'):
+        print("!send")
+        input = message.content.split()
+        if len(input) != 6:
+            await client.send_message(message.channel, 'Usage: !send <reqid> <wood> <clay> <iron> <crop>')
+            return
+        reqid = int(input[1])
+        swood = int(input[2])
+        sclay = int(input[3])
+        siron = int(input[4])
+        scrop = int(input[5])
+        query = """ INSERT INTO travian.requests (reqid, status) VALUES (%s, 'open') """ % (reqid)
+        session.execute (query)
+        query = """ select reqid, wood, woodsent, clay, claysent, iron, ironsent, crop, cropsent, player, village from travian.requests WHERE reqid = %s """ % (reqid)
+        results = session.execute (query)
+        for row in results:
+            reqid = row[0]
+            wood = row[1]
+            woodsent = row[2] + swood
+            clay = row[3]
+            claysent = row[4] + sclay
+            iron = row[5]
+            ironsent = row[6] + siron
+            crop = row[7]
+            cropsent = row[8] + scrop
+            player = row[9]
+            village = row[10]
+            query = """ INSERT INTO travian.requests (reqid, woodsent, claysent, ironsent, cropsent) VALUES (%s, %s, %s, %s, %s) """ % (int(reqid), woodsent, claysent, ironsent, cropsent)
+            session.execute (query)
+            await client.send_message(message.channel, "Request updated!")
 
     elif message.content.startswith('!new'):
       try:
@@ -149,6 +236,7 @@ async def on_message(message):
             await client.send_message(message.channel, 'Incorrect village coordinates!')
             await client.send_message(message.channel, 'ie: !res 1000 0 2000 1000 10,-10')
             return
+        village = xy[0] + "," + xy[1]
 
         query = """ UPDATE travian.counts set count = count + 1 where key = 'reqid' """ 
         session.execute (query)
@@ -168,4 +256,4 @@ async def on_message(message):
         await asyncio.sleep(5)
         await client.send_message(message.channel, 'Done sleeping')
 
-client.run('sdf.sdf.sdf')
+client.run('sdfsdfasdfsdfaDsdfsdfasdfsdfasdfsdfasdfsdfsdfsdfa')
